c25ca31e86a00c9719c6f1320e144ee4f07a7a95
pritamtaj <pritamtajpuriya@gmail.com>
2022-04-26 16:17:33 +0545

    BS date added to all pages

--- news/models.py
+++ news/models.py
@@ -23,6 +23,7 @@ class Category(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
